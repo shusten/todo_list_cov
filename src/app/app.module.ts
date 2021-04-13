b84075c7d6e213715ae335fa672e4ccd35370ee6
CovDev <shusten@hotmail.com>
2021-04-13 04:11:53 -0300

    :construction: chore: Adiciona SearchfilterPipe ao modulo da aplicaçao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {  ReactiveFormsModule } from '@angular/forms';
+import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
@@ -9,12 +9,19 @@ import { AppRoutingModule } from './/app-routing.module';
 import { TaskFormComponent } from './task-form/task-form.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { SharedModule } from './shared/shared.module';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { Ng2OrderModule } from 'ng2-order-pipe';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { SearchfilterPipe } from './searchfilter.pipe';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
     TaskFormComponent,
+    SearchfilterPipe,
   ],
   imports: [
     BrowserModule,
@@ -22,7 +29,11 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     ReactiveFormsModule,
     ModalModule.forRoot(),
-    SharedModule
+    SharedModule,
+    Ng2SearchPipeModule,
+    Ng2OrderModule,
+    NgxPaginationModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
